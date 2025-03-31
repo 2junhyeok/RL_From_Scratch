1dae2cc5bdf7694f347079a62537befeff966653
jh2020 <lgh0120ljh@gmail.com>
2025-03-31 20:02:46 +0900

    Feat: epsilon greedy and exp moving avg

--- MC/mc_control.py
+++ MC/mc_control.py
@@ -3,12 +3,14 @@ from collections import defaultdict
 class McAgent:
     def __init__(self):
         self.gamma = 0.9
+        self.epsilon = 0.1
+        self.alpha = 0.1
         self.action_size = 4
         
         random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
         self.pi = defaultdict(lambda: random_actions)
         self.Q = defaultdict(lambda: 0)
-        self.cnts = defaultdict(lambda: 0)
+        #self.cnts = defaultdict(lambda: 0)
         self.memory = []
     
     def get_action(self, state):
@@ -30,16 +32,18 @@ class McAgent:
             state, action, reward = data
             G = self.gamma * G + reward
             n = (state, action)
-            self.cnts[n] += 1# state, action 등장 횟수
-            self.Q[n] += (G - self.Q[n]) / self.cnts[n]# action value
-            
-            self.pi[state] = greedy_probs(self.Q, state)
+            #self.cnts[n] += 1# state, action 등장 횟수
+            #self.Q[n] += (G - self.Q[n]) / self.cnts[n]# action value
+            #self.pi[state] = greedy_probs(self.Q, state)
+            self.Q[n] += (G - self.Q[key]) / self.cnts[key]
+            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
             
     
-def greedy_probs(Q, state, action_size=4):
+def greedy_probs(Q, state, epsilon = 0, action_size=4):
     qs = [Q[(state, action)] for action in range(action_size)]
     max_action = np.argmax(qs)
     
-    action_probs = {action: 0.0 for action in range(action_size)}
-    action_probs[max_action] =1
-    return action_probs# greedy action 확률분포: {0: 1, 1: 0.0, 2: 0.0, 3: 0.0}
+    base_prob = epsilon / action_size
+    action_probs = {action: base_prob for action in range(action_size)}
+    action_probs[max_action] += (1 -epsilon)
+    return action_probs
