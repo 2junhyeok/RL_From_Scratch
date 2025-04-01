46d95961f92f60b5ad0f0742f74af2ced75bd2f5
jh2020 <lgh0120ljh@gmail.com>
2025-04-01 16:52:09 +0900

    Add: add a function of visualizing q_action

--- common/print_policy.py
+++ common/print_policy.py
@@ -14,3 +14,20 @@ def print_policy(pi):
     
     for row in grid:
         print('|' + '|'.join(f'{cell:^3}' for cell in row) + '|')
+
+def print_q_max(qs, env):
+    actions_map = {0: '↑', 1: '↓', 2: '←', 3: '→'}
+    height = env.height
+    width = env.width
+
+    grid_policy = [[' ' for _ in range(width)] for _ in range(height)]
+    grid_value = [[0.00 for _ in range(width)] for _ in range(height)]
+
+    for (state,action),q in qs.items():
+        if grid_value[state[0]][state[1]] <= q:
+            grid_value[state[0]][state[1]] = q
+            grid_policy[state[0]][state[1]] = actions_map[action]
+        else:
+            pass
+    for row in grid_policy:
+        print('|' + '|'.join(f'{cell:^3}' for cell in row) + '|')
