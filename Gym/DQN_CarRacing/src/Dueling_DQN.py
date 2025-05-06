6786dac4931133f8519dfdad0126776aa1f56191
jh2020 <lgh0120ljh@gmail.com>
2025-05-06 23:42:48 +0900

    Add: upload a Dueling DQN

---
+++ Gym/DQN_CarRacing/src/Dueling_DQN.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNDueling(nn.Module):
+    def __init__(self, state_dim, action_dim, activation=F.relu):
+        super(CNNDueling, self).__init__()
+        self.activation = activation
+        
+        self.conv1 = nn.Conv2d(state_dim, 16, kernel_size=8, stride=4)  # [N, 4, 84, 84] -> [N, 16, 20, 20]
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)  # [N, 16, 20, 20] -> [N, 32, 9, 9]
+        
+        self.in_features = 32 * 9 * 9
+        
+        self.adv_fc1 = nn.Linear(self.in_features, 256)
+        self.adv_fc2 = nn.Linear(256, action_dim)
+        
+        self.value_fc1 = nn.Linear(self.in_features, 256)
+        self.value_fc2 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))
+        x = x.view(x.size(0), -1)
+        
+        adv = self.activation(self.adv_fc1(x))
+        adv = self.adv_fc2(adv)
+        
+        value = self.activation(self.value_fc1(x))
+        value = self.value_fc2(value)
+        
+        Q = value + (adv - adv.mean(dim=1, keepdim=True))
+        
+        return Q
+
+class DuelingDQN:
+    def __init__(
+            self,
+            state_dim,
+            action_dim,
+            lr=0.00025,
+            epsilon=1.0,
+            epsilon_min=0.1,
+            gamma=0.99,
+            epsilon_decay=0.995,
+            batch_size=32,
+            warmup_steps=5000,
+            buffer_size=int(1e5),
+            target_update_interval=10000,
+    ):
+        self.action_dim = action_dim
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.gamma = gamma
+        self.batch_size = batch_size
+        self.warmup_steps = warmup_steps
+        self.target_update_interval = target_update_interval
+
+        self.network = CNNDueling(state_dim[0], action_dim)
+        self.target_network = CNNDueling(state_dim[0], action_dim)
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.optimizer = torch.optim.RMSprop(self.network.parameters(), lr)
+
+        self.buffer = ReplayBuffer(state_dim, (1,), buffer_size)
+        self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+        self.network.to(self.device)
+        self.target_network.to(self.device)
+
+        self.total_steps = 0
+
+    @torch.no_grad()
+    def act(self, x, training=True):
+        self.network.train(training)
+        if training and ((np.random.rand() < self.epsilon) or (self.total_steps < self.warmup_steps)):
+            a = np.random.randint(0, self.action_dim)
+        else:
+            
+            x = torch.from_numpy(x).unsqueeze(0).float().to(self.device)
+            
+            q = self.network(x)
+            a = torch.argmax(q).item()
+        return a
+
+    def learn(self):
+        s, a, r, s_prime, terminated = map(lambda x: x.to(self.device), self.buffer.sample(self.batch_size))
+
+        with torch.no_grad():
+            next_q_online = self.network(s_prime).detach()
+            next_actions = next_q_online.argmax(dim=1, keepdim=True)
+            
+            next_q_target = self.target_network(s_prime).detach()
+            next_q_values = next_q_target.gather(1, next_actions)
+            
+            td_target = r +(1 - terminated) * self.gamma * next_q_values
+            
+        current_q = self.network(s).gather(1, a.long())
+        loss = F.mse_loss(current_q, td_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        result = {
+            'total_steps': self.total_steps,
+            'value_loss': loss.item()
+        }
+        return result
+
+    def process(self, transition):
+        result = {}
+        self.total_steps += 1
+        self.buffer.update(*transition)
+
+        if self.total_steps > self.warmup_steps:
+            result = self.learn()
+
+        if self.total_steps % self.target_update_interval == 0:
+            self.target_network.load_state_dict(self.network.state_dict())
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            self.epsilon = max(self.epsilon, self.epsilon_min)
+        return result
+
+
+class ReplayBuffer:
+    def __init__(self, state_dim, action_dim, max_size=int(1e5)):
+        self.s = np.zeros((max_size, *state_dim), dtype=np.float32)
+        self.a = np.zeros((max_size, *action_dim), dtype=np.int64)
+        self.r = np.zeros((max_size, 1), dtype=np.float32)
+        self.s_prime = np.zeros((max_size, *state_dim), dtype=np.float32)
+        self.terminated = np.zeros((max_size, 1), dtype=np.float32)
+
+        self.ptr = 0
+        self.size = 0
+        self.max_size = max_size
+
+    def update(self, s, a, r, s_prime, terminated):
+        self.s[self.ptr] =s
+        self.a[self.ptr] =a
+        self.r[self.ptr] =r
+        self.s_prime[self.ptr] =s_prime
+        self.terminated[self.ptr] = terminated
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample(self, batch_size):
+        ind = np.random.randint(0, self.size, batch_size)
+        return (
+            torch.FloatTensor(self.s[ind]),
+            torch.FloatTensor(self.a[ind]),
+            torch.FloatTensor(self.r[ind]),
+            torch.FloatTensor(self.s_prime[ind]),
+            torch.FloatTensor(self.terminated[ind]),
+        )
