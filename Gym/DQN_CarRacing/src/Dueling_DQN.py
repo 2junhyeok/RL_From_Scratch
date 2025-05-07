7c327e7ccba1a348e33613018f0f3193cb5c7ca3
jh2020 <lgh0120ljh@gmail.com>
2025-05-07 20:10:41 +0900

    Chore: change gpu num

--- Gym/DQN_CarRacing/src/Dueling_DQN.py
+++ Gym/DQN_CarRacing/src/Dueling_DQN.py
@@ -67,7 +67,7 @@ class DuelingDQN:
         self.optimizer = torch.optim.RMSprop(self.network.parameters(), lr)
 
         self.buffer = ReplayBuffer(state_dim, (1,), buffer_size)
-        self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
         self.network.to(self.device)
         self.target_network.to(self.device)
 
